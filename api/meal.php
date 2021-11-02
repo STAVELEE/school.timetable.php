fd33d5fbae6318de085a2b37e7144b6444d78023
STAVELEE <91662692+STAVELEE@users.noreply.github.com>
2021-11-02 15:14:16 +0900

    footer 수정 및 refresh 다시 활성화

--- api/meal.php
+++ api/meal.php
@@ -60,6 +60,7 @@
             font-size: 28px;
             font-weight: 600;
         }
+        
     </style>
 
     <div class="px-4 py-5 my-5 text-center">
@@ -78,25 +79,10 @@
     <div class="container">
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
             <div class="col-md-4 d-flex align-items-center">
-                <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                    <svg class="bi" width="30" height="24">
-                        <use xlink:href="#bootstrap"></use>
-                    </svg>
-                </a>
                 <span class="text-muted">© 2021 Stave, Lee</span>
             </div>
 
-            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-                <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
-                            <use xlink:href="#twitter"></use>
-                        </svg></a></li>
-                <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
-                            <use xlink:href="#instagram"></use>
-                        </svg></a></li>
-                <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
-                            <use xlink:href="#facebook"></use>
-                        </svg></a></li>
-            </ul>
+            
         </footer>
     </div>
 </body>
